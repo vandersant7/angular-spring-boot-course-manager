823f24f2b40e352d993592be24d8b5e71fafc25c
Evandro Santos <vandersant7@gmail.com>
2024-09-02 17:42:30 -0300

    feat: add create course method to frontend

--- angular-course/src/app/courses/course-form/course-form.component.ts
+++ angular-course/src/app/courses/course-form/course-form.component.ts
@@ -1,12 +1,63 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: 'app-course-form',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatSnackBarModule
+  ],
   templateUrl: './course-form.component.html',
-  styleUrl: './course-form.component.scss'
+  styleUrl: './course-form.component.scss',
 })
 export class CourseFormComponent {
+  form: FormGroup;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private courseService: CoursesService,
+    private snackBar: MatSnackBar
+  ) {
+    this.form = this.formBuilder.group({
+      name: [null],
+      category: [null],
+    });
+  }
+
+  onSubmit() {
+    this.courseService.save(this.form.value).subscribe({
+      next: (result) => {
+        console.log(result);
+      },
+      error: () => {
+        this.onError('Erro ao salvar curso');
+      },
+    })
+  }
+
+  onCancel() {
+    this.form.reset();
+  }
+
+  private onError(errorMsg: string) {
+    this.snackBar.open(errorMsg, '', {
+      duration: 5000
+    })
+  }
 }
--- angular-course/src/app/courses/services/courses.service.ts
+++ angular-course/src/app/courses/services/courses.service.ts
@@ -4,24 +4,22 @@ import { Course } from '../model/course';
 import { delay, first, tap } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CoursesService {
-
   private readonly API = 'api/courses';
 
-  constructor(
-    private httpClient: HttpClient
-  ) { }
+  constructor(private httpClient: HttpClient) {}
 
   getCourses() {
-     return this.httpClient.get<Course[]>(this.API)
-     .pipe(
-      first(),//obtem a primeira resposta do servidor e finaliza a inscrição no end-point
+    return this.httpClient.get<Course[]>(this.API).pipe(
+      first(), //obtem a primeira resposta do servidor e finaliza a inscrição no end-point
       delay(1000),
-      tap(courses => console.log(courses))
+      tap((courses) => console.log(courses))
     );
+  }
 
-
+  save(record: Course) {
+    return this.httpClient.post<Course>(this.API, record);
   }
 }
